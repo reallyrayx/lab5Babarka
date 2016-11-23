d66794eb01f30f2642005e32349a030481dc3d7d
reallyrayx <Denver.5@mail.ru>
2016-11-24 02:31:55 +0300

    коммит2: переименован tabPage2

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -128,7 +128,7 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(260, 193);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "В Капс";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // button12
